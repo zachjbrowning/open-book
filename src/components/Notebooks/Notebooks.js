b9143124462f07beff52e9f38bdb11a8857bab1d
zachjbrowning <zachjbrowning@outlook.com>
2020-08-22 12:29:02 +1000

    landing setup

---
+++ src/components/Notebooks/Notebooks.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './Notebooks.module.scss';
+
+export default function Notebooks() {
+    const data = [
+        "COMP3511",
+        "data structurse",
+        "Yeet"
+    ]
+    
+    return <>
+        <div className={styles.welcome}>
+            <h1>Welcome, <span>zachary</span>!!</h1>        
+        </div>
+
+
+    </>
+
+}
--- src/components/Wireframe/Subframe.js
+++ src/components/Wireframe/Subframe.js
@@ -2,10 +2,25 @@ import React from 'react';
 import styles from './Wireframe.module.scss';
 import { Switch, Route } from 'react-router-dom';
 
+import Nav from '../Nav/Nav';
+import Notebooks from '../Notebooks/Notebooks';
 
 
 export default function Subframe() {
     return <>
         
+        <main className="container">
+
+            <div className="columns is-centered">
+                <div className={`${styles.card} column is-10`}>
+                    <Switch>
+                        <Route path="/notebooks" component={Notebooks} />
+                    </Switch>
+                    
+                </div>
+            </div>
+        </main>
+        <Nav />
+
     </>
 }
