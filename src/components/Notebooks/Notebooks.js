59e6c604b4965d21e36658066615d674b0619712
zachjbrowning <zachjbrowning@outlook.com>
2020-08-24 08:34:56 +1000

    state management more inline with new format

--- src/components/Notebooks/Notebooks.js
+++ src/components/Notebooks/Notebooks.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './Notebooks.module.scss';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+
+import { load_collection } from '../../../lib/redux/actions/collectionAction';
 
 export default function Notebooks() {
+    const dispatch = useDispatch();
+
+
+    useEffect(() => {
+        dispatch(load_collection());
+    }, [])
+    
     const data = [
         "COMP3511",
         "data structurse",
