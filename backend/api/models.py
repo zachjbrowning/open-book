b7be1c8e96ba950d842e3d3057c398b0c493685e
zachjbrowning <zachjbrowning@outlook.com>
2020-08-30 12:45:12 +1000

    django added

---
+++ backend/api/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    notes = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.title
---
+++ backend/api/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from rest_framework import viewsets
+
+from .serializers import NoteSerializer
+from .models import Note
+
+# Create your views here.
+
+
+class NoteViewSet(viewsets.ModelViewSet):
+    queryset = Note.objects.all().order_by('title')
+    serializer_class = NoteSerializer
