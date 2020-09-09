8b97c50b2fe6e9dbed70c8d9b346f9762826a4a8
zachjbrowning <zachjbrowning@outlook.com>
2020-09-09 13:29:41 +1000

    attempting to fix static issue

--- api/settings.py
+++ api/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
